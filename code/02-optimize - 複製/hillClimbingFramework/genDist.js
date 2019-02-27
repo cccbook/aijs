a8f80af8adecbb0426748e01389bb1227bc052e4
ccckmit <ccckmit@gmail.com>
2019-02-27 17:25:57 +0800

    modify hillClimbing Solution into class

--- code/02-optimize - 複製/hillClimbingFramework/genDist.js
+++
@@ -1,7 +0,0 @@
-var R = require("j6")
-
-console.log("rnorm(20,5,2)=", R.rnorm(100, 5, 2).str())
-console.log("rnorm(20,0,1)=", R.rnorm(100, 0, 1).str())
-console.log("rnorm(20,-2,2)=", R.rnorm(100, -2, 2).str())
-console.log("rnorm(20,3,1)=", R.rnorm(100, 3, 1).str())
-
--- code/02-optimize - 複製/hillClimbingFramework/hillClimbingCluster2.js
+++
@@ -1,8 +0,0 @@
-var hillClimbing = require("./hillClimbing");      // 引入爬山演算法類別
-var solutionCluster2 = require("../solution/solutionCluster2");    // 引入排課系統
-
-var hc = new hillClimbing();                       // 建立爬山演算法物件
-// 執行爬山演算法 (從「解答(x,y,z)=(1,1,1)」開始尋找, 最多十萬代、失敗一千次就跳出。
-var s = new solutionCluster2(solutionCluster2.init(2))
-// hc.run(s, 10, 1000)
-hc.run(s, 100000, 5000)
